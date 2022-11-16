9f970e50a6d4ce9bf3ca4bb84b81af465bcf5ca9
Sandagomi <sandagomitk@gmail.com>
2022-11-16 21:10:41 +0530

    jsx changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import {PostCard,Categories,PostWidget} from '../Components/Index'
 import Head from 'next/head'
 
 const posts = [
@@ -16,17 +17,19 @@ const Home: NextPage = () => {
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
+
       {posts.map((post,index) => (
-          <div> 
-            {post.title}
-            {post.excerpt}
-          </div>
+          <PostCard post={post} key={post.title}/>
         ))}
-        </div>
-      <div className='lg:col-span-4 col-span-1 '>
-        <div className='lg:sticky relative top-8'>
 
         </div>
+      <div className='lg:col-span-4 col-span-1 '>
+
+        <div className='lg:sticky relative top-8'>
+          <PostWidget />
+          <Categories/>
+        </div>
+
       </div>  
 
       </div>
