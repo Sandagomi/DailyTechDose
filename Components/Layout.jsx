211355f59d944635cf3c069967ea9d64b087d0da
Sandagomi <sandagomitk@gmail.com>
2022-11-17 15:19:00 +0530

    Header Changes

---
+++ Components/Layout.jsx
@@ -0,0 +1,13 @@
+import React, { children } from 'react'
+import { Header } from './Header'
+
+const Layout = ({children}) => {
+  return (
+    <>
+    <Header/>
+    {children}
+    </>
+  )
+}
+
+export default Layout
